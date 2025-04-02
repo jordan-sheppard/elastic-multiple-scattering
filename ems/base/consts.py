f83cdfa46efb4c3e47dd3c62ece19c8be80557e5
Jordan Sheppard <jg.sheppard808@gmail.com>
2025-04-02 14:41:11 -0600

    Refactored into package for multiple scattering (wrong branch, whoops)

---
+++ ems/base/consts.py
@@ -0,0 +1,53 @@
+import numpy as np
+from typing import Any
+from scipy.sparse.linalg import SuperLU
+from scipy.sparse import sparray
+from enum import Enum
+
+## ---------- Constants ----------
+# Number of spatial dimensions
+NUM_SPATIAL_DIMS = 2        
+
+# Number of angular gridpoints on each local polar grid
+NUM_ANGULAR_GRIDPOINTS = 50 
+
+## ---------- Custom types ----------
+Coordinates = tuple[float, float]
+Grid = np.ndarray[(int, int), float]
+Vector = np.ndarray[(int,), float]
+Matrix = np.ndarray[(int, int), float]
+SparseMatrix = sparray
+SparseMatrixLUDecomp = Any
+
+## ---------- Enums ----------
+class BoundaryCondition(Enum):
+    """Various boundary conditions that can be used at physical
+    boundaries
+    """
+    HARD = 1 
+    SOFT = 2
+
+class Algorithm(Enum):
+    """Various boundary conditions that can be used at physical
+    boundaries
+    """
+    GAUSS_SEIDEL = 1
+
+class QOI(Enum):
+    """Quantities of interest"""
+    DISPLACEMENT = 1 
+    STRESS = 2
+    POTENTIALS = 3
+
+class CoordinateSystem(Enum):
+    """Coordinate systems used for finding vector/tensor quantities."""
+    LOCAL_POLAR = 1 
+    LOCAL_CARTESIAN = 2
+    GLOBAL_POLAR = 3
+    GLOBAL_CARTESIAN = 4
+
+class ErrorType(Enum):
+    L2 = 1
+    L2_RELATIVE = 2 
+    LINFTY = 3
+
