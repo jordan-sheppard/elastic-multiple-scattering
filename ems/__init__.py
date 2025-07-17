0e06abdf8cf81fe6cd23d25caa02e2cee2d772d8
Jordan Sheppard <jg.sheppard808@gmail.com>
2025-07-17 16:44:52 -0700

    Edits to global namespace to make only the most useful things available there

--- ems/__init__.py
+++ ems/__init__.py
@@ -1,7 +1,5 @@
 # Most necessary high-level things for running experiments imported here.
 from .fd.algorithm import MKFE_FD_ScatteringProblem, ScatteringConvergenceAnalyzerPolar
-from .fd.obstacles import Circular_MKFE_FDObstacle
-from .fd.waves import IncidentPlanePWave, IncidentPlanePWaveEvaluator
 from .fd.plotting import ScatteringProblemPlotter
-from .base.consts import BoundaryCondition, PlotType, Algorithm
+from .base.consts import Algorithm, ComplexArrayQuantity
 from .base.text_parsing import get_full_configuration_filename_base
